9e7e9edb84870d0ce312970b35bfa545521932e5
Ali Utku Selen <auselen@gmail.com>
2014-04-26 20:49:01 +0200

    Background bending

--- src/se/dolkow/graphbender/scene/backgrounds/PictureBackground.java
+++ src/se/dolkow/graphbender/scene/backgrounds/PictureBackground.java
@@ -6,18 +6,23 @@ import se.dolkow.graphbender.ui.Background;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class PictureBackground implements Background {
 
 	private Bitmap mBackground;
+	private Rect srcRect;
 
 	public PictureBackground() {
 		mBackground = BitmapFactory.decodeResource(Globals.sAppResources, R.drawable.starrynight);
+		srcRect = new Rect(0, 0, mBackground.getWidth(), mBackground.getHeight());
+		
 	}
 	
 	@Override
 	public void draw(Canvas c, long frameTime, long deltaTime) {
-		c.drawBitmap(mBackground, 0, 0, null);
+		c.drawBitmap(mBackground, srcRect,
+				new Rect(0, 0, c.getWidth(), c.getHeight()), null);
 	}
 
 	@Override
