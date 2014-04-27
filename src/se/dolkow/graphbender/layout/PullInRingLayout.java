f410f6aef058c426f5a07d37e40cb10cd2dfb2aa
Snild Dolkow <snild@dolkow.se>
2014-04-27 13:27:34 +0200

    Adjust margins for PullInRingLayout

--- src/se/dolkow/graphbender/layout/PullInRingLayout.java
+++ src/se/dolkow/graphbender/layout/PullInRingLayout.java
@@ -1,12 +1,15 @@
 package se.dolkow.graphbender.layout;
 
 import static se.dolkow.graphbender.util.Utils.linterp;
+import se.dolkow.graphbender.Metric;
 import se.dolkow.graphbender.logic.Logic;
 import se.dolkow.graphbender.logic.Vertex;
 
 public class PullInRingLayout implements Layout {
 
-	private static final int MARGIN = 50;
+	// can't have these static, because class may be loaded before SCALE is set..
+	private final int MARGIN = (int)(40 * Metric.SCALE);
+	private final int MARGIN_TOP_EXTRA = (int)(20 * Metric.SCALE);
 
 	@Override
     public void updateDesiredPositions(Logic logic, final int width, final int height) {
@@ -17,7 +20,7 @@ public class PullInRingLayout implements Layout {
 			final int targetCount = connCount + v.getRequired(); 
 			double r = linterp(1, 0.1, logic.getConnectionCount(i), 0, targetCount);
 			v.targetX = (int)( width/2 + ( width/2 - MARGIN) * r * Math.cos(i*2*Math.PI / N));
-			v.targetY = (int)(height/2 + (height/2 - MARGIN) * r * Math.sin(i*2*Math.PI / N));
+			v.targetY = (int)(height/2 + (height/2 - MARGIN - MARGIN_TOP_EXTRA) * r * Math.sin(i*2*Math.PI / N)) + MARGIN_TOP_EXTRA;
 		}
     }
 	
